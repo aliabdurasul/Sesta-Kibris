bdc4f5cc12bbc10903f520d4108977ac5441dfe8
aliabdurasul <s0023884@gmail.com>
2026-05-12 13:21:03 +0300

    routing1.1

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,6 @@ const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
   outputFileTracingRoot: path.join(__dirname),
   images: {
     unoptimized: true,
